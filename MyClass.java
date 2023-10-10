11fd74edd3acfaa5ffdf5d6789032383cdb92782
AlessandroCiaccio <146941441+AlessandroCiaccio@users.noreply.github.com>
2023-10-10 18:04:12 +0200

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -3,6 +3,6 @@ public class MyClass {
       double x=5.0;
       double y=8.0;
       
-      System.out.println("The numbers are " + (int) x + " and " + (int) y);
+      System.out.println("The sum is " + (int) (x+y));
     }
 }
