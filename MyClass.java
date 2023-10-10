9cf485db5e3b93724cf838a2fa303aaab3f9e4e0
Alessandro Scrofani <sandroscro@libero.it>
2023-10-10 17:55:18 +0200

    Added DoubleToInt

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+      double x=5.0;
+      double y=8.0;
+      
+      System.out.println("The numbers are " + (int) x + " and " + (int) y);
+    }
+}
